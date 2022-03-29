562e9662af13c270e1a4a232dd46a884f04bd63a
willo <willowtown0576@gmail.com>
2022-03-29 19:52:50 +0900

    テンプレート修正

--- lib/common.sh
+++ lib/common.sh
@@ -1,38 +1,65 @@
 #!/bin/bash
+#-*- coding: utf-8 -*-
 
-readonly EXIT_NORMAL=0
-readonly EXIT_WARNING=4
-readonly EXIT_ERROR=8
+readonly _BASE_DIR_="$(cd $(dirname $0)/.. && pwd)"
+readonly _SUCCESS_=0
+readonly _WARNING_=1
+readonly _ERROR_=2
+readonly _CRITICAL=4
+readonly _TRUE_=0
+readonly _FALSE_=1
 
-function common::start_logging() {
-  local _log_file # ログ出力先ファイル(フルパス)
+# Name:
+#   common::logging - ログ出力関数
+# Usage:
+#   common::logging message
+# Options:
+#   None
+# Dependency:
+#   None
+# Returns:
+#   0 on success
+#   non zero on error
+function common::logging() {
+  local _msg
+  _msg="$1"
+  _msg="$(date '+%Y/%m/%d %H:%M:%S') - $(basename $0) - ${_msg}"
+  logger "${_msg}"
+  echo "${_msg}" 1>&2
+}
 
-  while getopts d:h OPT; do
-    case ${OPT} in
-      d)
-        _log_file=$OPTARG
-        ;;
-      h)
-        cat << EOF
-Usage:
-  common::start_logging -d log_file [-h]
-EOF
-        exit "${EXIT_WARNING}"
-        ;;
-      *)
-        echo "${FUNCNAME[0]}の引数に誤りがあります"
-        exit "${EXIT_ERROR}"
-        ;;
-    esac
-  done
+# Name:
+#   common::parse_json - JSONファイル読み取り関数
+# Usage:
+#   common::parse_json filter json
+# Options:
+#   None
+# Dependency:
+#   jq
+# Returns:
+#   0 on success
+#   non zero on error
+function common::parse_json() {
+  local _filter
+  local _json
+  _filter="$1"
+  _json="$2"
 
-  if [[ ! -d $(dirname "${_log_file}") ]]; then
-    # shellcheck disable=SC2086
-    echo "ログ出力先:$(dirname ${_log_file})/が存在しません"
-    exit "${EXIT_ERROR}"
+  # jqコマンドがインストールされているかを確認
+  if ! type jq > /dev/null 2>&1; then
+    common::logging "jqコマンドがインストールされていません"
+    exit ${_CRITICAL_}
   fi
 
-  export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME:+${FUNCNAME}(): }'
-  exec 2> "${_log_file}"
-  set -ux
+  if [[ ! -f "${_json}" ]]; then
+    common::logging "${_json}が存在しません"
+    exit ${_ERROR_}
+  fi
+
+  if ! jq -r "${_filter}" "${_json}"; then
+    common::logging "jqコマンドに失敗しました。実行コマンド: jq -r ${_filter} ${_json}"
+    exit ${_ERROR_}
+  fi
+
+  return ${_SUCCESS_}
 }
